46da49038f1c14b221ef31a3d8036a8daf14ab71
SoupsnSpoons <143634308+SoupsnSpoons@users.noreply.github.com>
2025-09-12 18:31:36 -0400

    09/12/25 additions
    The last windows have been put in place and the door has been created. Larry has also been moved into place and now we just have to get the lamppost in place. We're amazing.

--- Picture.java
+++ Picture.java
@@ -1,13 +1,12 @@
 /**
- * This class represents a simple picture. You can draw the picture using
- * the draw method. But wait, there's more: being an electronic picture, it
- * can be changed. You can set it to black-and-white display and back to
- * colors (only after it's been drawn, of course).
- *
+ * This is an image of a man coming home to his apartment after a long day.
+ * Two windows are lit, the rest are dark, as most people are asleep at this time of night. 
+ * A single lamppost outside illuminates the apartment building.
+ * 
  * This class was written as an early example for teaching Java with BlueJ.
  * 
- * @author  Michael Kšlling and David J. Barnes
- * @version 2016.02.29
+ * @author  Arielle Bowie
+ * @version September 22nd, 2025
  */
 public class Picture
 {
@@ -18,6 +17,9 @@ public class Picture
     private Square window3;
     private Square window4;
     private Square window5;
+    private Square window6;
+    private Square door;
+    private Square door2;
     private Circle sun;
     private Circle sun2;
     private Person larry;
@@ -35,6 +37,9 @@ public class Picture
         window3 = new Square();
         window4 = new Square();
         window5 = new Square();
+        window6 = new Square();
+        door = new Square();
+        door2 = new Square();
         sun = new Circle();
         sun2 = new Circle();
         larry = new Person();
@@ -62,35 +67,54 @@ public class Picture
             
             window.changeColor("yellow");
             window.moveHorizontal(-280);
-            window.moveVertical(-70);
+            window.moveVertical(-90);
             window.changeSize(30);
             window.makeVisible();
             
             window2.changeColor("yellow");
             window2.moveHorizontal(-200);
-            window2.moveVertical(-70);
+            window2.moveVertical(-90);
             window2.changeSize(30);
             window2.makeVisible();
             
             //make window3 dark grey once you get the colors working
             window3.changeColor("yellow");
             window3.moveHorizontal(-280);
-            window3.moveVertical(0);
+            window3.moveVertical(-20);
             window3.changeSize(30);
             window3.makeVisible();
             
             window4.changeColor("yellow");
             window4.moveHorizontal(-200);
-            window4.moveVertical(0);
+            window4.moveVertical(-20);
             window4.changeSize(30);
             window4.makeVisible();
             
             //make window5 dark grey once you get the colors working
             window5.changeColor("yellow");
             window5.moveHorizontal(-280);
-            window5.moveVertical(70);
+            window5.moveVertical(50);
             window5.changeSize(30);
             window5.makeVisible();
+            
+            //make window6 dark grey once you get the colors working
+            window6.changeColor("yellow");
+            window6.moveHorizontal(-200);
+            window6.moveVertical(50);
+            window6.changeSize(30);
+            window6.makeVisible();
+            
+            door.changeColor("white");
+            door.moveHorizontal(-240);
+            door.moveVertical(110);
+            door.changeSize(30);
+            door.makeVisible();
+            
+            door2.changeColor("white");
+            door2.moveHorizontal(-240);
+            door2.moveVertical(140);
+            door2.changeSize(30);
+            door2.makeVisible();
     
             sun.changeColor("yellow");
             sun.moveHorizontal(100);
@@ -109,7 +133,7 @@ public class Picture
             larry.changeColor("magenta");
             larry.changeSize(60,30);
             larry.moveHorizontal(-30);
-            larry.moveVertical(25);
+            larry.moveVertical(60);
             larry.makeVisible();
             drawn = true;
         }
@@ -127,6 +151,9 @@ public class Picture
         window3.changeColor("black");
         window4.changeColor("black");
         window5.changeColor("black");
+        window6.changeColor("black");
+        door.changeColor("black");
+        door2.changeColor("black");
         sun.changeColor("white");
         sun2.changeColor("white");
         larry.changeColor("white");
@@ -141,12 +168,17 @@ public class Picture
         //make sky color dark blue once you get the colors working
         sky.changeColor("blue");
         window.changeColor("yellow");
+        //make window2 dark grey once you get the colors working
         window2.changeColor("yellow");
         //make window3 dark grey once you get the colors working
         window3.changeColor("yellow");
         window4.changeColor("yellow");
         //make window5 dark grey once you get the colors working
         window5.changeColor("yellow");
+        //make window6 dark grey once you get the colors working
+        window6.changeColor("yellow");
+        door.changeColor("white");
+        door2.changeColor("white");
         sun.changeColor("yellow");
         sun2.changeColor("green");
         larry.changeColor("magenta");
@@ -158,14 +190,17 @@ public class Picture
     /*public void setForegroundColor(String colorString)
     {
         if (colorString.equals("red")){
-            graphic.setColor(new Color(235,25,25));
+            graphic.setColor(new.Color(235,25,25));
         }
         else if (colorString.equals("darkblue")){
-            graphic.setColor(new Color (18,15,85));
+            graphic.setColor(new.Color(18,15,85));
         }
         else if (colorString.equals("darkgrey")){
-            graphic.setColor(new Color (99,99,99));
+            graphic.setColor(new.Color(99,99,99));
         }
-	}*/
+        else if (colorString.equals("lightgrey")){
+            graphic.setColor(new.Color(190,190,190));
+        }
+    }*/
     }
 
